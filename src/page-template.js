0708836843823d7b42da5e2a307fec46002722cf
FrankShock <schockeyf@gmail.com>
2021-12-06 12:14:13 -0600

    created/worked on pagetemplate

---
+++ src/page-template.js
@@ -0,0 +1,20 @@
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Team Profile Generator</title>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body class = "bg-silver">
+     <div class = "container-fluid bg-silver">
+     <div class="col-12 jumbotron bg-blue text-white mb-3">
+         <h1 class="head-text text-center">Team Layout</h1>
+         </div>
+        </div>
+    </div>
